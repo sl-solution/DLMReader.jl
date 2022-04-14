c7306a29e1e9535523f29fab28fe8f273f42db71
Documenter.jl <documenter@juliadocs.github.io>
2022-04-14 11:44:04 +0000

    build based on 42478c5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DLMReader.jl","page":"Introduction","title":"DLMReader.jl","text":"","category":"section"},{"location":"#Package-manual","page":"Introduction","title":"Package manual","text":"","category":"section"},{"location":"","page":"Introduction","title":"Introduction","text":"Pages = []\nDepth = 2","category":"page"},{"location":"#API","page":"Introduction","title":"API","text":"","category":"section"}]
+}
